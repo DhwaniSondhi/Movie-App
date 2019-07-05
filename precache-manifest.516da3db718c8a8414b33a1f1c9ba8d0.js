6022c112bfdfcc55e979870363b2131630bf87ae
DSondhi <dhwanisondhi3@gmail.com>
2019-07-05 04:35:52 -0400

    Updates

---
+++ precache-manifest.516da3db718c8a8414b33a1f1c9ba8d0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "898d6a06ee23a577e3c4fc1c1e97e2d1",
+    "url": "/Movie-App/index.html"
+  },
+  {
+    "revision": "61216406cd5e74d43f55",
+    "url": "/Movie-App/static/css/main.3cb8837a.chunk.css"
+  },
+  {
+    "revision": "3e85aa7afde212d29acc",
+    "url": "/Movie-App/static/js/2.93030805.chunk.js"
+  },
+  {
+    "revision": "61216406cd5e74d43f55",
+    "url": "/Movie-App/static/js/main.fbd562bf.chunk.js"
+  },
+  {
+    "revision": "7198c6a1e3704149705a",
+    "url": "/Movie-App/static/js/runtime~main.c4615995.js"
+  }
+]);
